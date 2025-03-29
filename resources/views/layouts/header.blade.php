3074cf1a46e10322028c7f734a6fe2622e890926
Murcilton <tutstuts.belka@mail.ru>
2025-03-29 22:04:46 +0500

    Header 1

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,22 @@
+<header>
+    <div class="logo">
+        <img src="{{asset('storage/GUI/Logo.svg')}}" alt="">
+    </div>
+    
+    <nav class="nav">
+        <button class="btnheader">Создать объявление</button>
+        <button class="btnheader">О нас</button>
+        <button class="btnheader">Контакты</button>
+    </nav>
+
+    <div class="search">
+        <i class="fa-solid fa-magnifying-glass"></i>
+        <input type="text" class="searchInp" placeholder="">
+        <i class="fa-solid fa-caret-left"></i>
+    </div>
+
+    <div class="avatar">
+        <img src="{{asset('storage\img\e8aaf878c512cc898bf7a6479e553657.448x448x1.png')}}" alt="">
+    </div>
+
+</header>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -8,24 +7,12 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/css/app.css',
             ],
             refresh: true,
         }),
         
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
     ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-        },
-    },
     server: {
         hmr: {
             host: 'localhost',

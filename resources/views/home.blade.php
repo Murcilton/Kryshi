41716cf3521198d3488226dcf49eae04d4c25bad
Murcilton <tutstuts.belka@mail.ru>
2025-04-02 16:29:20 +0500

    Home 2 (cards)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,6 +38,55 @@
         </div>
     </div>
 
+    <div class="cards-container">
+        <div class="card card-left">
+            <img class="theLine" src="{{asset('storage\GUI\cardsHome\TheLine.svg')}}" alt="">
+            <img class="card-img" src="{{asset('storage\GUI\cardsHome\image.png')}}" alt="">
+            <div class="content">
+                <div class="title">
+                    Личный подход <i class="fa-solid fa-handshake-simple"></i>
+                </div>
+                <div class="description">
+                    <p>Юридическое сопровождение</p>
+                    <p><span>7</span> ступеней проверки</p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="card card-middle">
+            <img class="theLine" src="{{asset('storage\GUI\cardsHome\TheLine.svg')}}" alt="">
+            <img class="card-img" src="{{asset('storage\GUI\cardsHome\image (1).png')}}" alt="">
+            <div class="content">
+                <div class="title">
+                    Охват всей страны <i class="fa-solid fa-city"></i>
+                </div>
+                <div class="description">
+                    <p>Мы в <span>25</span> городах России!</p>
+                    <p><span>150+</span> жилых комплексов</p>
+                    <p><span>40+</span> партнёров</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card card-right">
+            <img class="theLine" src="{{asset('storage\GUI\cardsHome\TheLine.svg')}}" alt="">
+            <img class="card-img" src="{{asset('storage\GUI\cardsHome\image (2).png')}}" alt="">
+            <div class="content">
+                <div class="title">
+                    Работаем с 2010 года <i class="fa-solid fa-star"></i>
+                </div>
+                <div class="description">
+                    <p><span>84%</span> положительных обзоров</p>
+                    <p>Больше <span>10 000</span> сделок</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="cabinet">
+        <button class="cabinetBtn">Войти в личный кабинет <i class="fa-solid fa-id-card"></i><i class="fa-solid fa-chevron-right"></i> </button>
+    </div>
+
 </div>
 
 @endsection

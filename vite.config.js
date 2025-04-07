d0c2150f282df0750dd28cfb2a0cd583fd643fb2
Nikita8993 <nikitamaskin802@gmail.com>
2025-04-07 20:11:59 +0500

    1

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,10 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
                 'resources/css/app.css',
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
-        
     ],
-    server: {
-        hmr: {
-            host: 'localhost',
-        },
-        cors: {
-            origin: 'http://localhost', 
-            credentials: true,
-        },
-    },
 });
